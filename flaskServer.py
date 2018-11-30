65a80a3b424e1cbc282e1bd3250567397a25d8d9
jasonxia17 <jasonx3@illinois.edu>
2018-11-29 18:47:03 -0600

    Added a placeholder response for testing

--- flaskServer.py
+++ flaskServer.py
@@ -8,15 +8,26 @@ def handleClueRequest():
     import time
     t0 = time.time()
     jsonContent = request.get_json()
+    print(jsonContent)
     result = getClues(jsonContent['words'], jsonContent['labels'], jsonContent['team'])
     mostHintedAt = max([i for i in range(len(result)) if result[i]])
     print(mostHintedAt)
     print(time.time() - t0)
-    return jsonify(result[mostHintedAt][0])
+    return jsonify(result)
+    # placeholder = {
+    #     "clue": "sky",
+    #     "rating": 0.891205757856369,
+    #     "wordsHintedAt": [
+    #         "air",
+    #         "eye",
+    #         "Jupiter"
+    #     ]
+    # }
+    # return jsonify(placeholder)
 
 @app.route('/<name>', methods=['GET', 'POST'])
 def hello_name(name):
     return name + ' is not a valid route'
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0')
