54f878ccc44d445ca3de429d51e738fa4e278fd6
jasonxia17 <jasonx3@illinois.edu>
2018-12-06 12:16:27 -0600

    Filter cluesOnBoard, handle upper/lowercase and multi-word phrases

--- cluegiver.py
+++ cluegiver.py
@@ -3,27 +3,32 @@ from operator import itemgetter
 filename = 'GoogleNews-vectors-negative300.bin.gz'
 model = KeyedVectors.load_word2vec_format(filename, binary=True, limit=200000)
 
-def getWordsHintedAt(clue, goodWords, badWords, neutralWords, assassin):
+def getWordsHintedAt(clue, goodWords, badWords, neutralWords, assassins):
     # similar to scoring in curling
-    maxBadCloseness = max([model.similarity(clue, badWord) for badWord in badWords])
-    maxNeutralCloseness = max([model.similarity(clue, word) for word in neutralWords])
-    assassinCloseness = model.similarity(clue, assassin)
+    # Append 0 to avoid bugs due to empty lists (i.e. no more neutral words)
+    maxBadCloseness = max([model.similarity(clue, badWord) for badWord in badWords] + [0])
+    maxNeutralCloseness = max([model.similarity(clue, word) for word in neutralWords] + [0])
+    assassinCloseness = max([model.similarity(clue, word) for word in assassins] + [0])
 
     maxAllowedCloseness = max(maxBadCloseness + 0.1, maxNeutralCloseness - 0.15, assassinCloseness + 0.2)
 
     return [goodWord for goodWord in goodWords
             if model.similarity(clue, goodWord) > maxAllowedCloseness]
 
-def clueSearch(potentialClues, goodWords, badWords, neutralWords, assassin):
-    
-    # maxWordsHintedAt = max([len(getWordsHintedAt(clue, goodWords, badWords, assasin))
-    #                         for clue in potentialClues])
-    
+def clueOnBoard(clue, wordList):
+    for word in wordList:
+        if clue.lower() in word.lower() or word.lower() in clue.lower():
+            return True
+    return False
+
+def clueSearch(potentialClues, goodWords, badWords, neutralWords, assassins):
     cluesByNumHintedAt = [[] for _ in range(10)]
     clueObjectList = []
     for clue in potentialClues:
-        wordsHintedAt = getWordsHintedAt(clue, goodWords, badWords, neutralWords, assassin)
-        if len(wordsHintedAt) < 2:
+        if clueOnBoard(clue, goodWords + badWords + neutralWords + assassins):
+            continue
+        wordsHintedAt = getWordsHintedAt(clue, goodWords, badWords, neutralWords, assassins)
+        if len(wordsHintedAt) == 0:
             continue
         
         hintRatings = [model.similarity(clue, hintedAt) for hintedAt in wordsHintedAt]
@@ -51,11 +56,18 @@ def getClues(wordObjectList, team):
     redWords = []
     blueWords = []
     neutralWords = []
-    assassin = ""
+    assassins = []
 
     for wordObject in wordObjectList:
         word = wordObject['word']
-        label = wordObject['label']
+        word = '_'.join(word.split())
+        if word.lower() in model:
+            word = word.lower()
+        elif word.title() in model:
+            word = word.title()
+        else:
+            raise ValueError('Invalid Word')
+
         if wordObject['stillOnBoard']:
             label = wordObject['label']
             if label == 'R':
@@ -65,18 +77,22 @@ def getClues(wordObjectList, team):
             elif label =='N':
                 neutralWords.append(word)
             elif label == 'A':
-                assassin = word
+                assassins.append(word)
 
     print(redWords)
     print(blueWords)
     print(neutralWords)
-    print(assassin)
+    print(assassins)
     print(team)
 
     potentialClues = list(model.vocab.keys())[:10000]
 
     if team == 'red':
-        searchResults = clueSearch(potentialClues, redWords, blueWords, neutralWords, assassin)
+        searchResults = clueSearch(potentialClues, redWords, blueWords, neutralWords, assassins)
     else:
-        searchResults = clueSearch(potentialClues, blueWords, redWords, neutralWords, assassin)
+        searchResults = clueSearch(potentialClues, blueWords, redWords, neutralWords, assassins)
     return searchResults
+
+# while True:
+#     word = input()
+#     print(word in model)
